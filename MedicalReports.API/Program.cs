49e6e17f4195948eba6030177e521f27c3e48a02
paulo.fonseca <paulo.fonseca@practocol.com>
2025-11-10 10:06:42 +0100

    Added automatic database migrations on startup

--- MedicalReports.API/Program.cs
+++ MedicalReports.API/Program.cs
@@ -41,4 +41,11 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+// Apply migrations automatically
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    dbContext.Database.Migrate();
+}
+
 app.Run();
